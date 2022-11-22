686c02ac579643fec9ad051739baa018e3c74475
Ninhem <bertrandnicolas343@gmail.com>
2022-11-22 16:38:51 +0100

    new carousel

--- public/js/carrousel.js
+++ public/js/carrousel.js
@@ -1,36 +1,52 @@
-// 1 : Select Carousel Element
-const carouselHome = document.getElementById('carousel-content');
-// console.log(carouselHome);
-    // Output : Return the HTML section with this id : carousel-products-home
+var slides = document.querySelectorAll('.slide');
+    var btns = document.querySelectorAll('.btn');
+    let currentSlide = 1;
 
-// 2 : Get Array of Element include in #carousel-content & Lenght(in number of element) of carousel element and set it to a variable
-const productBoxElements = Array.from(carouselHome.children);
-// console.log(carouselHomeArray);
-    // Output : Return an array of each element in the HTML section with the id carousel-content
-const carouselHomeLength = carouselHome.children.length;
-// console.log(carouselHomeLength);
-    // Output : Number of children in carousel
+    // Javascript for image slider manual navigation
+    var manualNav = function(manual){
+      slides.forEach((slide) => {
+        slide.classList.remove('active');
 
-// 3 : Get the width of the carousel element
-let allProductBoxWidth = [];
-productBoxElements.forEach(productBoxItem => {
-    let productBoxWidth = productBoxItem.offsetWidth + 40;
-    allProductBoxWidth.push(productBoxWidth);
-});
-// console.log(allProductBoxWidth);
-    // Output : Number of pixels without margin ==> Array of width of each element in the carousel (in number of pixels)
+        btns.forEach((btn) => {
+          btn.classList.remove('active');
+        });
+      });
 
-// 4 : Animation of carousel :
-// Select buttons to control carousel
-const prevBtn = document.getElementById('prev-btn');
-const nextBtn = document.getElementById('next-btn');
+      slides[manual].classList.add('active');
+      btns[manual].classList.add('active');
+    }
 
-// Add an Eventlistener to change the scroll position of the carousel element
-prevBtn.addEventListener('click', () => {
-    carouselHome.scrollLeft -= 340;
+    btns.forEach((btn, i) => {
+      btn.addEventListener("click", () => {
+        manualNav(i);
+        currentSlide = i;
+      });
+    });
 
-});
+    // Javascript for image slider autoplay navigation
+    var repeat = function(activeClass){
+      let active = document.getElementsByClassName('active');
+      let i = 1;
 
-nextBtn.addEventListener('click', () => {
-    carouselHome.scrollLeft += 340;
-});
+      var repeater = () => {
+        setTimeout(function(){
+          [...active].forEach((activeSlide) => {
+            activeSlide.classList.remove('active');
+          });
+
+        slides[i].classList.add('active');
+        btns[i].classList.add('active');
+        i++;
+
+        if(slides.length == i){
+          i = 0;
+        }
+        if(i >= slides.length){
+          return;
+        }
+        repeater();
+      }, 10000);
+      }
+      repeater();
+    }
+    repeat();
