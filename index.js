ff778027f03dd7d7296593e5a195d85bfd16d13d
Ninhem <bertrandnicolas343@gmail.com>
2022-12-09 11:36:00 +0100

    server page principale

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ app.use(express.static('public'));
 app.listen(80,
     () => console.log("server listening on localhost:80")
     );
+
+// GET method route
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/Accueil.html');
+  });
+
+  
