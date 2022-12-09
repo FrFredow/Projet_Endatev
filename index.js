2135710afd8cb624bacb61663bf58604efde7ccc
FrFredow <serrierefrederic14@gmail.com>
2022-12-09 15:45:09 +0100

    update file index

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const app = express()
 
 app.use(express.static('public'));
 
-app.listen(80,
-    () => console.log("server listening on localhost:80")
-    );
+app.listen(80,() => console.log("server listening on localhost:80"));
 
 // GET method route
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/Accueil.html');
-  });
+});
 
 app.use((req,res, next) => {
     res.status(404).sendFile(__dirname + '/public/html/Erreur_404.html')
