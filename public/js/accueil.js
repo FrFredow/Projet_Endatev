74c50625e6fac120dbc9cdfc6cc335ba822a2780
FrFredow <serrierefrederic14@gmail.com>
2022-12-09 15:04:23 +0100

    merge with new arbo

---
+++ public/js/accueil.js
@@ -0,0 +1,30 @@
+let slides = document.querySelectorAll('.slide');
+let btns = document.querySelectorAll('.btn');
+let currentSlide = "";
+
+let manualNav = function () {
+
+}
+ 
+// // Javascript for image slider manual navigat	n
+// let manualNav = function (
+// 		manualslides.forEach(
+// 			(slide) = slide.classList.remove('active',			    
+// 			btns.forEach(
+// 				(btn) = btn.classList.remove('active');		    
+//                 slides[manual].classList.add('active');
+//                 btns[manual].classList.add('active');	;
+//                 btns.forEach((btn, i) =	btn.addEventListener("click", () = manualNav(currentSlide ="";	
+
+//   // Javascript for image slider autoplay navigat	n
+// var repeat = function (activeClass		    
+//     let active = document.getElementsByClassName('active		    
+//     let i = 		    
+//     var repeater = () =	setTimeout(function ([...active].forEach((activeSlide) = activeSlide.classList.remove('active')
+//     slides[i].classList.add('active				    btns[i].classList.add('active				    i+				    
+//     if (slides.length == i =				  				    
+//         if (i >= slides.length	
+//             retu repeater
+//         }, 1000 repeater;
+//     }
+// repeat();
