9286f6bb291573c7b06e30811e9a724661f96096
Ninhem <bertrandnicolas343@gmail.com>
2023-01-06 17:26:04 +0100

    petit changement pop-up

--- public/js/changement_texte_pop_up.js
+++ public/js/changement_texte_pop_up.js
@@ -1,15 +1,57 @@
 const categorie = document.getElementsByClassName("categorie")
-const paragraphe = document.getElementById("texte-changeant1")
+const paragraphe1 = document.getElementById("texte-changeant1")
+const paragraphe2 = document.getElementById("texte-changeant2")
+const paragraphe3 = document.getElementById("texte-changeant3")
+const paragraphe4 = document.getElementById("texte-changeant4")
 
 categorie[0].addEventListener("click", () => {
-    paragraphe.innerHTML = "Les horaires sont de ... a ...";
+    paragraphe1.innerHTML = "Les horaires sont de 1h a 2h";
 })
 categorie[1].addEventListener("click", () => {
-    paragraphe.innerHTML = "Ce job est rémunéré à ... euros";
+    paragraphe1.innerHTML = "Ce job est rémunéré à ... euros";
 })
 categorie[2].addEventListener("click", () => {
-    paragraphe.innerHTML = "Les avantages sont: ";
+    paragraphe1.innerHTML = "Les avantages sont: ";
 })
 categorie[3].addEventListener("click", () => {
-    paragraphe.innerHTML = "Ce job consiste en ... ";
+    paragraphe1.innerHTML = "Ce job consiste en ... ";
+})
+
+categorie[0].addEventListener("click", () => {
+    paragraphe2.innerHTML = "Les horaires sont de 3h a 4h";
+})
+categorie[1].addEventListener("click", () => {
+    paragraphe2.innerHTML = "Ce job est rémunéré à ... euros";
+})
+categorie[2].addEventListener("click", () => {
+    paragraphe2.innerHTML = "Les avantages sont: ";
+})
+categorie[3].addEventListener("click", () => {
+    paragraphe2.innerHTML = "Ce job consiste en ... ";
+})
+
+categorie[0].addEventListener("click", () => {
+    paragraphe3.innerHTML = "Les horaires sont de 5h a 6h";
+})
+categorie[1].addEventListener("click", () => {
+    paragraphe3.innerHTML = "Ce job est rémunéré à ... euros";
+})
+categorie[2].addEventListener("click", () => {
+    paragraphe3.innerHTML = "Les avantages sont: ";
+})
+categorie[3].addEventListener("click", () => {
+    paragraphe3.innerHTML = "Ce job consiste en ... ";
+})
+
+categorie[0].addEventListener("click", () => {
+    paragraphe4.innerHTML = "Les horaires sont de 7h a 8h";
+})
+categorie[1].addEventListener("click", () => {
+    paragraphe4.innerHTML = "Ce job est rémunéré à ... euros";
+})
+categorie[2].addEventListener("click", () => {
+    paragraphe4.innerHTML = "Les avantages sont: ";
+})
+categorie[3].addEventListener("click", () => {
+    paragraphe4.innerHTML = "Ce job consiste en ... ";
 })
